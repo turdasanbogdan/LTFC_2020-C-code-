eb03f263218767b3445fb06b9c5f98d95a22da42
Student <student@debian.intranet.cs.upt.ro>
2020-03-09 16:00:27 +0200

    added new files

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "alex.h"
+
+#include "main.h"
 char * pch;
-enum{ID, BREAK, CHAR, DOUBLE, ELSE, FOR, IF , INT , REUTRN, STRUCT, VOID, WHILE, 
-CT_INT, CT_REAL,CT_CHAR, CT_STRING,
-COMMA, SEMICOLON, LPAR, RPAR, LBRACKET,RBRACKET, LACC,RACC,
-ADD, SUB, MUL, DIV, DOT, AND, OR, NOT, ASSIGN, EQUAL, NOTEQ,LESS,LESSEQ,GREATER,GREATEREQ,
-SPACE, LINECOMMENT, COMMENT, UNKNOWN};
+
+void err(const char *fmt, ...){
+    va_list va;
+    va_start(va, fmt);
+    fprintf(stderr,"error:");
+    vfprintf(stderr, fmt, va);
+    fputc('\n',stderr);
+    exit(-1);
+}
+
+
 int main(int argc, char **argv){
-     
+
+    
     FILE *f = fopen(argv[1], "rb");
     if (!f) {
         perror("Eroare la deschiderea fisierului");
@@ -21,6 +27,7 @@ int main(int argc, char **argv){
     
     rewind(f);
 
+    Token *tk;
     char *buff;
     buff = (char *)malloc(size*sizeof(char));
     if(!buff){
@@ -33,7 +40,7 @@ int main(int argc, char **argv){
     pch = buff;
     printf("In fisier avem ceva de genu: %s", pch);
 
-    getState(pch);
+    getNextToken(pch,tk);
 
 
     return 0;
